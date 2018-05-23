d51ced4b40a5ac1c439c9c26414f37feacbfede1
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-23 18:41:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  return string.toLowerCase
+}
+
+var sayHiToGrandma ="HELLO"
+if (sayHiToGrandma === "HELLO") {
+  console.log("I can't hear you!")
+}
