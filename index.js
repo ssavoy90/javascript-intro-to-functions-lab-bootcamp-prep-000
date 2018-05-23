2427e69f3c066d2f455b0b60df43a25a01f0b83f
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-23 19:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
    return "I can't hear you!" 
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else{
+  } else if (string ==="I love you, Grandma")
     return "I love you, Grandma"
   }
 }
